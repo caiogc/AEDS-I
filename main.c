f2f2d49284bff3e4d6b6abd759a3376e468b4502
caiogc <52391175+caiogc@users.noreply.github.com>
2019-08-27 11:14:45 -0300

    Criacao de bibliotecas

--- main.c
+++ main.c
@@ -1,68 +1,68 @@
 #include <stdio.h>
 #include <stdlib.h>
 #define TAM 50
+#include "vendas.h"
+#include "produto.h"
+#include "cliente.h"
 
-typedef struct modulo
+
+typedef struct data
 {
-    typedef struct data
-    {
-        int dia;
-        int mes;
-        int ano;
-    } TData;
+    int dia;
+    int mes;
+    int ano;
+} TData;
 
-    typedef struct produto
-    {
-        int codigo;//id
-        char nome[TAM];
-        char descricao[150];
-        TData dataFabricacao;
-        int lote;
-        int quantidade;
-        float preco;
+typedef struct produto
+{
+    int codigo;//id
+    char nome[TAM];
+    char descricao[150];
+    TData dataFabricacao;
+    int lote;
+    int quantidade;
+    float preco;
 
-    } TProduto;
+} TProduto;
 
-    typedef struct endereco
-    {
-        char rua[TAM];
-        int numero;
-        int complemento;
-        char bairro[TAM];
-        char cidade[TAM];
-        int cep;
+typedef struct endereco
+{
+    char rua[TAM];
+    int numero;
+    int complemento;
+    char bairro[TAM];
+    char cidade[TAM];
+    int cep;
 
-    } TEndereco;
+} TEndereco;
 
 
 
-    typedef struct cliente
-    {
-        char id[TAM];// CPF/CNPJ
-        int tipo; //0:física, 1:jurídica
-        TEndereco enderecoCompleto;
-        TData dataNascimento;
-        char nome[TAM];
+typedef struct cliente
+{
+    char id[TAM];// CPF/CNPJ
+    int tipo; //0:física, 1:jurídica
+    TEndereco enderecoCompleto;
+    TData dataNascimento;
+    char nome[TAM];
 
-    } TCliente;
+} TCliente;
 
 
-    typedef struct vendas
-    {
-        char ID[TAM];
-        int codigo;
-        TData dataVenda;
-        int tipo; //0: à vista, 1: à prazo
-        TData dataPagamento;
+typedef struct vendas
+{
+    char ID[TAM];
+    int codigo;
+    TData dataVenda;
+    int tipo; //0: à vista, 1: à prazo
+    TData dataPagamento;
 
-    } TVenda;
-
-} TModulo;
+} TVenda;
 
 void lerProduto(TModulo)
 {
-    printf("Codigo:/t");
-    scanf("%d",&TModulo.TProduto.codigo);
+printf("Codigo:/t");
+scanf("%d",&TModulo.TProduto.codigo);
 }
 void lerCliente()
 {
@@ -76,8 +76,15 @@ void lerVendas()
 
 int main()
 {
-    lerProduto(TModulo);
-    printf("%d",TModulo.TProduto.codigo);
+//declaracao de variaveis auxiliares
+TProduto produto1;
+TCliente cliente1;
+TVenda venda1;
 
-    return 0;
+//Modulos
+TModuloProduto modulo1;
+TModuloCliente modulo2;
+TModuloVenda modulo3;
+
+return 0;
 }
